31e5492791d98bddc22036b794fa721b86cd4093
ambiennt <63216972+ambiennt@users.noreply.github.com>
2021-02-21 11:23:54 -0800

    optimize death.js and fix bug

--- death.js
+++ death.js
@@ -3,25 +3,30 @@ const system = server.registerSystem(0, 0);
 system.initialize = function() {
     this.listenForEvent(`minecraft:entity_death`, (event) => {
 
-        const deadentity = system.getComponent(event.data.entity, "minecraft:nameable");
-        const killerentity = system.getComponent(event.data.killer, "minecraft:nameable");
-        const deadentitypos = system.getComponent(event.data.entity, "minecraft:position");
-        const { x, y, z } = deadentitypos.data;
+        const deadEntity = system.getComponent(event.data.entity, "minecraft:nameable");
+        const killerEntity = system.getComponent(event.data.killer, "minecraft:nameable");
+        const deadEntityPos = system.getComponent(event.data.entity, "minecraft:position");
+        const { x, y, z } = deadEntityPos.data;
 
-        //runs if a player dies no matter what
+        //runs if a player dies no matter what - gravestone still works if player dies in lava
         if (event.data.entity.__identifier__ === "minecraft:player") {
-            system.executeCommand(`summon uhc:gravestone ${Math.round(x)} ${Math.round(y)} ${Math.round(z)}`, () => {});
+        	system.executeCommand(`fill ${x-5} ${y-3} ${z-5} ${x+5} ${y+3} ${z+5} air 0 replace lava`, () => {});
+        	system.executeCommand(`fill ${x-5} ${y-3} ${z-5} ${x+5} ${y+3} ${z+5} air 0 replace fire`, () => {});
+        	system.executeCommand(`setblock ${Math.floor(x)} ${Math.floor(y-1)} ${Math.floor(z)} bedrock`, () => {});
+            system.executeCommand(`summon uhc:gravestone ${Math.floor(x)} ${Math.floor(y)} ${Math.floor(z)}`, () => {});
 
-            //runs if a player dies, but not to another player ... /kill seems to not trigger this
-            if (event.data.entity.__identifier__ === "minecraft:player" && event.data.killer.__identifier__ !== "minecraft:player") {
-                system.executeCommand(`tellraw @a {"rawtext":[{"text":"§c${deadentity.data.name} was slain at position: ${Math.round(x)}, ${Math.round(y)}, ${Math.round(z)}§r"}]}`, () => {});
+            //if a player was killed by another player
+            if (event.data.killer && event.data.killer.__identifier__ === "mineraft:player") {
+                const killerHealth = system.getComponent(event.data.killer, "minecraft:health");
+                system.executeCommand(`tellraw @a {"rawtext":[{"text":"§c${deadEntity.data.name} was slain by ${killerEntity.data.name} §a(${killerHealth.data.value}HP) §cat position: ${Math.round(x)}, ${Math.round(y)}, ${Math.round(z)}§r"}]}`, () => {});
+                system.executeCommand(`execute ${killerEntity.data.name} ~ ~ ~ function killer`, () => {});
             }
-            //runs if a player dies to another player
-            if (event.data.entity.__identifier__ === "minecraft:player" && event.data.killer.__identifier__ === "minecraft:player") {
-                const killerhealth = system.getComponent(event.data.killer, "minecraft:health");
-                system.executeCommand(`tellraw @a {"rawtext":[{"text":"§c${deadentity.data.name} was slain by ${killerentity.data.name} §a(${killerhealth.data.value}HP) §cat position: ${Math.round(x)}, ${Math.round(y)}, ${Math.round(z)}§r"}]}`, () => {});
-                system.executeCommand(`execute ${killerentity.data.name} ~ ~ ~ function killer`, () => {});
+
+            //runs if a player dies, but not to another player
+            else {
+                system.executeCommand(`tellraw @a {"rawtext":[{"text":"§c${deadEntity.data.name} was slain at position: ${Math.round(x)}, ${Math.round(y)}, ${Math.round(z)}§r"}]}`, () => {});
             }
         }
     });
 }
+console.log("death.js loaded");
