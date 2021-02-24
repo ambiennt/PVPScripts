0bbd4b5bb13580d48113cfb87c25bcbee7dfafac
ambiennt <63216972+ambiennt@users.noreply.github.com>
2021-02-23 19:31:27 -0800

    update to filter on sword as damage source

--- attack_particles/attack_particles.js
+++ attack_particles/attack_particles.js
@@ -5,13 +5,17 @@ system.listenForEvent("minecraft:player_attacked_entity", ({data: eventData}) =>
 		player: entity,
 		attacked_entity: attackedEntity
 	} = eventData
-
-	let particleEventData = system.createEventData( "minecraft:spawn_particle_attached_entity" )
-	particleEventData.data.effect = "ambient:damage_emitter"//custom particle
-	particleEventData.data.offset = [ 0, 1.2, 0 ]
-	particleEventData.data.entity = attackedEntity
 	
-	if (attackedEntity.__identifier__ !== "minecraft:player") return;//comment this line out for particles on all entities
-	system.broadcastEvent("minecraft:spawn_particle_attached_entity", particleEventData)
+	let handContainer = system.getComponent(entity, "minecraft:hand_container");
+	let mainhandItem = handContainer.data[0];
+
+	if (attackedEntity.__identifier__ === "minecraft:player" && mainhandItem.item.includes("sword")) {
+
+		let particleEventData = system.createEventData( "minecraft:spawn_particle_attached_entity" )
+		particleEventData.data.effect = "ambient:damage_emitter"//custom particle
+		particleEventData.data.offset = [ 0, 1.2, 0 ]
+		particleEventData.data.entity = attackedEntity
+		system.broadcastEvent("minecraft:spawn_particle_attached_entity", particleEventData)
+	};
 })
 console.log("attack_particles.js loaded");
