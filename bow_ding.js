b81c481fbe4f33871fe325f48fb7b36826f76c02
ambiennt <63216972+ambiennt@users.noreply.github.com>
2021-03-20 18:55:59 -0700

    update to use projectile hit event

--- bow_ding.js
+++ bow_ding.js
@@ -1,24 +1,23 @@
 const system = server.registerSystem(0, 0);
 
-system.listenForEvent("minecraft:entity_hurt", ({ data: eventData }) => {
-    const { attacker, entity, projectile_type } = eventData;
+system.listenForEvent("minecraft:projectile_hit", ({ data: eventData }) => {
+    const { owner, entity, projectile } = eventData;
 
-    if (attacker.__identifier__ == "minecraft:player" && entity.__identifier__ == "minecraft:player" && projectile_type == "arrow") {
-        const attackerName = system.getComponent(attacker, "minecraft:nameable").data.name;
-        system.executeCommand(`execute "${attackerName}" ~ ~ ~ playsound random.orb @s ~ ~ ~ 0.4 0.5`, () => {});
-        OnNextTickSend(entity, attackerName);
-    }
+    if (owner.__identifier__ === "minecraft:player" && entity.__identifier__ === "minecraft:player" && projectile.__identifier__ === "minecraft:arrow") {
+        const ownerName = system.getComponent(owner, "minecraft:nameable").data.name;
+        system.executeCommand(`execute "${ownerName}" ~ ~ ~ playsound random.orb @s ~ ~ ~ 0.4 0.5`, () => {});
+        OnNextTickSend(entity, ownerName);
+    };
 });
 
 //get attacked entity health after taking damage
-function OnNextTickSend(entity, attackerName) {
-    let tick = false; {
-        system.update = function() {
-            if (tick == false) {
-                const entityHealth = system.getComponent(entity, "minecraft:health").data.value;
-                const entityName = system.getComponent(entity, "minecraft:nameable").data.name;
-                system.executeCommand(`tellraw "${attackerName}" {"rawtext":[{"text":"§e${entityName} is at §c${entityHealth} §ehealth!§r"}]}`, () => { tick = true; });
-            }
-        };
-    }
-}
+function OnNextTickSend(entity, ownerName) {
+    let tick = false;
+    system.update = function() {
+      if (tick == false) {
+        const entityHealth = system.getComponent(entity, "minecraft:health").data.value;
+        const entityName = system.getComponent(entity, "minecraft:nameable").data.name;
+        system.executeCommand(`tellraw "${ownerName}" {"rawtext":[{"text":"§e${entityName} is at §c${entityHealth} §ehealth!§r"}]}`, () => { tick = true; });
+    };
+  };
+};
