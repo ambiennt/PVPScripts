fa3b3df3c67c3e9372ae26f68961c723e89d86f0
ambiennt <63216972+ambiennt@users.noreply.github.com>
2021-06-05 21:38:32 -0700

    Update attack_particles.js

--- attack_particles/attack_particles.js
+++ attack_particles/attack_particles.js
@@ -9,13 +9,13 @@ system.listenForEvent("minecraft:player_attacked_entity", ({data: eventData}) =>
 	let handContainer = system.getComponent(entity, "minecraft:hand_container");
 	let mainhandItem = handContainer.data[0];
 
-	if (attackedEntity.__identifier__ === "minecraft:player" && mainhandItem.item.includes("sword")) {
+	//check if attacked entity is a player and the attacker has a sword in their main hand
+	if (attackedEntity.__identifier__ == "minecraft:player" && mainhandItem.item.includes("sword")) {
 
 		let particleEventData = system.createEventData( "minecraft:spawn_particle_attached_entity" )
-		particleEventData.data.effect = "ambient:damage_emitter"//custom particle
-		particleEventData.data.offset = [ 0, 1.2, 0 ]
+		particleEventData.data.effect = "ambient:hit_particle"//custom particle
+		particleEventData.data.offset = [ 0, 0.5, 0 ]
 		particleEventData.data.entity = attackedEntity
 		system.broadcastEvent("minecraft:spawn_particle_attached_entity", particleEventData)
 	};
 })
-console.log("attack_particles.js loaded");
