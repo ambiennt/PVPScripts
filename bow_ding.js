06e167a6240da9ccd50e77442240534a309d8df5
ambiennt <63216972+ambiennt@users.noreply.github.com>
2021-06-05 21:37:48 -0700

    Update bow_ding.js

--- bow_ding.js
+++ bow_ding.js
@@ -3,21 +3,21 @@ const system = server.registerSystem(0, 0);
 system.listenForEvent("minecraft:projectile_hit", ({ data: eventData }) => {
     const { owner, entity, projectile } = eventData;
 
-    if (owner.__identifier__ === "minecraft:player" && entity.__identifier__ === "minecraft:player" && projectile.__identifier__ === "minecraft:arrow") {
+    if (owner.__identifier__ == "minecraft:player" && entity.__identifier__ == "minecraft:player" && projectile.__identifier__ == "minecraft:arrow") {
         const ownerName = system.getComponent(owner, "minecraft:nameable").data.name;
-        system.executeCommand(`execute "${ownerName}" ~ ~ ~ playsound random.orb @s ~ ~ ~ 0.4 0.5`, () => {});
-        OnNextTickSend(entity, ownerName);
+        system.executeCommand(`execute "${ownerName}" ~ ~ ~ playsound random.orb @s ~ ~ ~ 0.375 0.5`, () => {});
+        let sendTellraw = true;
+        OnNextTickSend(sendTellraw, entity, ownerName);
     };
 });
 
 //get attacked entity health after taking damage
-function OnNextTickSend(entity, ownerName) {
-    let tick = false;
+function OnNextTickSend(sendTellraw, entity, ownerName) {
     system.update = function() {
-      if (tick == false) {
+      if (sendTellraw) {
         const entityHealth = system.getComponent(entity, "minecraft:health").data.value;
         const entityName = system.getComponent(entity, "minecraft:nameable").data.name;
-        system.executeCommand(`tellraw "${ownerName}" {"rawtext":[{"text":"§e${entityName} is at §c${entityHealth} §ehealth!§r"}]}`, () => { tick = true; });
+        system.executeCommand(`tellraw "${ownerName}" {"rawtext":[{"text":"§e${entityName} is at §c${entityHealth} §ehealth!§r"}]}`, () => { sendTellraw = false; });
     };
   };
 };
