c52c3f47ac93a9bb7ee1b96486e5932ae8001b44
ambiennt <63216972+ambiennt@users.noreply.github.com>
2021-07-03 02:12:31 -0700

    Update bow_ding.js
    prevent activation if player shoots him/herself with arrow

--- bow_ding.js
+++ bow_ding.js
@@ -5,19 +5,22 @@ system.listenForEvent("minecraft:projectile_hit", ({ data: eventData }) => {
 
     if (owner.__identifier__ == "minecraft:player" && entity.__identifier__ == "minecraft:player" && projectile.__identifier__ == "minecraft:arrow") {
         const ownerName = system.getComponent(owner, "minecraft:nameable").data.name;
+        const entityName = system.getComponent(entity, "minecraft:nameable").data.name;
+        //exit script when a player shoots themselves with an arrow
+        if (ownerName == entityName) return;
         system.executeCommand(`execute "${ownerName}" ~ ~ ~ playsound random.orb @s ~ ~ ~ 0.375 0.5`, () => {});
         let sendTellraw = true;
         OnNextTickSend(sendTellraw, entity, ownerName);
-    };
+    }
 });
 
 //get attacked entity health after taking damage
 function OnNextTickSend(sendTellraw, entity, ownerName) {
     system.update = function() {
-      if (sendTellraw) {
-        const entityHealth = system.getComponent(entity, "minecraft:health").data.value;
-        const entityName = system.getComponent(entity, "minecraft:nameable").data.name;
-        system.executeCommand(`tellraw "${ownerName}" {"rawtext":[{"text":"§e${entityName} is at §c${entityHealth} §ehealth!§r"}]}`, () => { sendTellraw = false; });
+        if (sendTellraw) {
+            const entityHealth = system.getComponent(entity, "minecraft:health").data.value;
+            const entityName = system.getComponent(entity, "minecraft:nameable").data.name;
+            system.executeCommand(`tellraw "${ownerName}" {"rawtext":[{"text":"§e${entityName} is at §c${entityHealth} §ehealth!§r"}]}`, () => { sendTellraw = false; });
+        }
     };
-  };
 };
